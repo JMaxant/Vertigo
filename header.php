6f4ed28b81bb66fe8a05da6b72409e28e1cc9661
Luu-Steven <33942988+Luu-Steven@users.noreply.github.com>
2018-01-16 15:39:50 +0100

    Add files via upload
    test header + slider bootstrap

--- header.php
+++ header.php
@@ -23,7 +23,38 @@
 </head>
 <body>
 <header>
-	<nav>
+	<nav class="navbar">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <img class="navbar-brand" src="test.svg" alt="Le Groupe Vertigo">
+	    </div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SPECTACLES <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	          </ul>
+	        <li class="active"><a href="#">DATES <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">NOUS</a></li>
+	        <li><a href="#">WORKSHOPS</a></li>
+	        </li>
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">FR</a></li>
+	        <li><a href="#">EN</a></li>
+	      </ul>
+	    </div>
+	  </div>
+		<div>
   				<?php
 			wp_nav_menu( array(
 				'menu' =>'top-menu',
@@ -39,4 +70,31 @@
 			 ?>
   		</div>
 	</nav>
+	<div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="1.jpg" alt="...">
+	    </div>
+	    <div class="item">
+	      <img src="2.jpg" alt="...">
+	    </div>
+	    <div class="item">
+	      <img src="3.jpg" alt="...">
+	    </div>
+	    <div class="item">
+	      <img src="4.jpg" alt="...">
+	    </div>
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
 </header>
