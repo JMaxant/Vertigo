ec83194346918e37accda622ee315dbb58dbe81f
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-01-17 14:52:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<?php get_header(); ?>
-	<section>
-		<?php if(have_posts()): ?>
-		<div class="container">
-			<?php while(have_posts()):the_post(); ?>
-				<?php get_template_part('content');?>
-			<?php endwhile;?>
-	<?php else: ?>
-			<div class="row">
-				<div class="col-xs-12">
-					<p>Pas de résultat</p>
-				</div>
-			</div><!--row-->
-	<?php endif; ?>
-		</div><!--Container-->
-	</section>
-
-<?php get_footer();?>
-</html>
+<?php
+get_header();
+echo do_shortcode('[twabc-carousel category="index"]');
+get_footer();
+?>
