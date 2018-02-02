eb971a3392e7df7040a6bc23950993e76381433d
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-02 17:31:44 +0100

    Update page-workshops.php

--- page-workshops.php
+++ page-workshops.php
@@ -2,10 +2,20 @@
 /*
 Template Name: Workshops
 */
-php the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ));
-?>
-	<h1> <?php the_title(); ?></h1>
+get_header(); ?>
+<figure>
+     <?php the_post_thumbnail('full', array('class'=>'img-responsive'));?>
+</figure>
+<section id="workshop" class="container">
+     <div class="row">
+          <h1><?php the_title();?></h1>
+          <?php if (have_posts()){
+               while(have_posts()){
+                    the_post();
+                    the_content();
+               }
+          } ?>
+     </div>
+</section>
 
-<?php
-
- ?>
+<?php get_footer(); ?>
