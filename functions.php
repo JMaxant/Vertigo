ec9cab6876550b3b5891035cf5fb2fb848f5d2d9
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-02 17:30:17 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,33 +2,17 @@
 /*************CHARGEMENT DES SCRIPTS/CSS****************************/
 /*****************Préfixe perso GV = Groupe Vertigo **********************/
 define('GV_VERSION','1.0.0');
-
 //CHARGEMENT FRONT
 function gv_scripts(){
 // CHARGEMENT CSS
 	wp_enqueue_style('gv_bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), GV_VERSION, 'all' );
 	wp_enqueue_style('gv_custom', get_template_directory_uri() . '/style.css', array('gv_bootstrap-core'), GV_VERSION, 'all' );
-
-
 // CHARGEMENT JS
 	wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'), GV_VERSION, true);
 	wp_enqueue_script('gv_custom_js', get_template_directory_uri().'/js/gv_script.js', array('jquery', 'bootstrap-js'), GV_VERSION, true);
 } //Fin fonction gv_scripts
-
 add_action('wp_enqueue_scripts', 'gv_scripts');
-
-
-
-// CHARGEMENT BACK
-function gv_admin_scripts() {
-	// wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri().'/css/bootstrap.min.css', array(),GV_VERSION);
-}
-//FIN FONCTION gv_admin_scripts
-add_action('admin_init','gv_admin_scripts' );
-
-
 /************SETUP UTILITAIRES****************/
-
 function gv_setup(){
 	// AJOUT IMAGE A LA UNE
 	add_theme_support('post-thumbnails');
@@ -44,7 +28,6 @@ function gv_setup(){
 	register_nav_menus( array( 'primary' => 'Principal', 'secondary' =>'Secondaire', 'footer' =>'Footer'));
 }
 /********************AFFICHAGE DATES + CATEGORIES*********************/
-
 function gv_postMeta($date1, $date2, $cat, $tags){
 	$chaine='publié le <time class="entry-date" datetime="';
 	$chaine.=$date1;
@@ -53,19 +36,14 @@ function gv_postMeta($date1, $date2, $cat, $tags){
 	$chaine.='</time> dans la catégorie';
 	$chaine.=$cat;
 	$chaine.= ' avec les étiquettes : '.$tags;
-
 	return $chaine;
 }
 add_action('after_setup_theme', 'gv_setup');
-
 /*****************MODIF TEXTE EXCERPTS + READ MORE*****************/
 function gv_ReadMore($more){
 	return '<a class="more-link" href="'.get_permalink().'">'.'En savoir plus'.'</a>';
 }
 add_filter('excerpt_more', 'gv_ReadMore');
-
-
-
 /*****************CPT******************/
 /*pieces*/
 function wpm_custom_post_type() {
@@ -83,9 +61,7 @@ function wpm_custom_post_type() {
 		'not_found'           => __( 'Non trouvée'),
 		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
 	);
-
 	// On peut définir ici d'autres options pour notre custom post type
-
 	$args = array(
 		'label'               => __( 'Pièces'),
 		'description'         => __( 'Tous sur nos pièces'),
@@ -99,12 +75,8 @@ function wpm_custom_post_type() {
 		'public'              => true,
 		'has_archive'         => true,
 		'rewrite'			  => array( 'slug' => 'pieces'),
-
 	);
-
 	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
 	register_post_type( 'pieces', $args );
-
 }
-
 add_action( 'init', 'wpm_custom_post_type', 0 );  ?>
