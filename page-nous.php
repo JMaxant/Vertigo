fb1422d74802df84b144c5878a4ae2b1a6852720
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-04 00:01:05 +0100

    Update page-nous.php

--- page-nous.php
+++ page-nous.php
@@ -13,14 +13,17 @@ get_header(); ?>
 	<div class="row">
 	<?php
         $thefields=get_field_objects();
+	   // var_dump($thefields);
 	if(!empty ($thefields)) { ?>
 		<ul class="nav nav-tabs">
 		<?php $i=0;
-		foreach($thefields as $name => $value) { ?>
-			<li <?php if ($i === 0) {echo 'class="active"' ;} ?> >
-				<a href="#<?php echo $name ?>" data-toggle="tab"><?php echo $value[label]; ?></a>
-			</li>
+		foreach($thefields as $name => $value) {
+				if(!empty ($value[value])){ ?>
+					<li <?php if ($i === 0) {echo 'class="active"' ;} ?> >
+						<a href="#<?php echo $name ?>" data-toggle="tab"><?php echo $value[label]; ?></a>
+					</li>
 			<?php $i++;
+				}
 			}
 		} ?>
 		</ul>
@@ -28,7 +31,7 @@ get_header(); ?>
 			<?php if(!empty($thefields)){
 				$i=0;
 				foreach($thefields as $name => $value) {?>
-			<div class="tab-pane <?php if ($i === 0) {echo "active"; } ?>" id="<?php echo $name ?>" role="tabpanel">
+			<div class="tab-pane<?php if ($i === 0) {echo " "."active"; } ?>" id="<?php echo $name ?>" role="tabpanel">
 				<article>
 					<?php echo $value[value]; ?>
 				</article>
