857862dfb701fb9a2af464176bfc069c50c53660
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-07 12:18:26 +0100

    Update single-pieces.php

--- single-pieces.php
+++ single-pieces.php
@@ -5,26 +5,23 @@ Template Post Type: post, pieces
 */
 get_header(); ?>
 <figure>
-	<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive' )); ?>
+	<?php the_post_thumbnail( 'headerPieces', array( 'class' => 'img-responsive' )); ?>
 </figure>
+<header>
+    	<h1><?php the_title(); ?></h1>
+</header>
 <section id="pieces" class="container">
-	<header>
-		<h1><?php the_title(); ?></h1>
-	</header>
 	<div class="row">
 	<?php
         $thefields=get_field_objects();
-	   // var_dump($thefields);
 	if(!empty ($thefields)) { ?>
 		<ul class="nav nav-tabs">
 		<?php $i=0;
-		foreach($thefields as $name => $value) {
-				if(!empty ($value[value])){ ?>
-					<li <?php if ($i === 0) {echo 'class="active"' ;} ?> >
-						<a href="#<?php echo $name ?>" data-toggle="tab"><?php echo $value[label]; ?></a>
-					</li>
+		foreach($thefields as $name => $value) { ?>
+			<li <?php if ($i === 0) {echo 'class="active"' ;} ?> >
+				<a href="#<?php echo $name ?>" data-toggle="tab"><?php echo $value[label]; ?></a>
+			</li>
 			<?php $i++;
-				}
 			}
 		} ?>
 		</ul>
