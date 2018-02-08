49715f26380218cc64fc984763764928a8ca629f
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-08 10:40:02 +0100

    Update page-workshops.php

--- page-workshops.php
+++ page-workshops.php
@@ -9,13 +9,22 @@ get_header(); ?>
 <section id="workshop" class="container">
      <div class="row">
           <h1><?php the_title();?></h1>
-          <?php if (have_posts()){
-               while(have_posts()){
-                    the_post();
-                    the_content();
-               }
-          } ?>
+          <?php if(have_rows('contenu')){
+               while(have_rows('contenu')){
+                    the_row();
+                    $image=get_sub_field('img');
+                    ?>
+                    <article class="content col-md-12">
+                         <figure class="col-sm-12 col-md-4">
+                              <img class="img-responsive" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']?>"/>
+                         </figure>
+                         <div class="col-sm-12 col-md-8"><?php the_sub_field('txt'); ?></div>
+                    </article>
+               <?php  } // fin while
+          } // fin if
+?>
      </div>
 </section>
 
+<?php // var_dump($image) ?>
 <?php get_footer(); ?>
