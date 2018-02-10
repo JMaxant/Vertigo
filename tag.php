cd5cb9a6169ebbeb693eadc64b21212bbd8a80fa
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-10 20:23:45 +0100

    Update tag.php

--- tag.php
+++ tag.php
@@ -1,23 +1,2 @@
-<?php get_header(); ?>
-	<section>
-		<div class="container">
-			<div class="row">
-				<p class="lead">Liste des articles avec l'étiquette  <?php single_tag_title(); ?></p>
-			</div>
-		</div>
-		<div class="container">
-			<?php if(have_posts()): ?>
-				<?php while(have_posts()):the_post(); ?>
-					<?php get_template_part('content');?>
-				<?php endwhile;?>
-				<?php else: ?>
-			<div class="row">
-				<div class="col-xs-12">
-					<p>Pas de résultat</p>
-				</div>
-			</div><!--row-->
-		<?php endif; ?>
-		</div><!--Container-->
-	</section>
+<?php get_header();?>
 <?php get_footer();?>
-</html>
