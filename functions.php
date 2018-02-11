ed250f993261b7104d6ede55e8e20df577b5dab0
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-11 21:54:38 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@ define('GV_VERSION','1.0.0');
 function gv_scripts(){
 // CHARGEMENT CSS
 	wp_enqueue_style('gv_bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), GV_VERSION, 'all' );
-	wp_enqueue_style('gv_custom', get_template_directory_uri() . '/style.css', array('gv_bootstrap-core'), GV_VERSION, 'all' );	
+	wp_enqueue_style('gv_custom', get_template_directory_uri() . '/style.css', array('gv_bootstrap-core'), GV_VERSION, 'all' );
+
+
 // CHARGEMENT JS
 	wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'), GV_VERSION, true);
 	wp_enqueue_script('gv_custom_js', get_template_directory_uri().'/js/gv_script.js', array('jquery', 'bootstrap-js'), GV_VERSION, true);
@@ -15,10 +17,8 @@ function gv_scripts(){
 
 add_action('wp_enqueue_scripts', 'gv_scripts');
 /***********TAILLE D'IMAGES PERSONALISEES************************/
-if (function_exists( 'add_image_size'  ) {
-	add_image_size( 'thumbSpectacles', 400, 400, true );/* Taille personalisées pour les vignettes page_spectacles*/
-	add_image_size( 'headerPieces', 1920, 930, true );/* Taille personalisées pour les vignettes page_spectacles*/
-}
+	add_image_size( 'thumbSpectacles', 400, 400, true); /* Taille personalisées pour les vignettes page_spectacles*/
+	add_image_size( 'headerPieces', 1920, 930, true); /* Taille personalisées pour les vignettes single_pièces */
 /************SETUP UTILITAIRES****************/
 
 function gv_setup(){
@@ -62,12 +62,15 @@ function formatDate($date){
 	$moisChiffre=['01','02','03','04','05','06','07','08','09','10','11','12'];
 	$moisLettre=['janvier', 'février', 'mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
 	$date=explode('-', $date);
-	$moisReplace=$date[1];
-	$mois=str_replace($moisChiffre, $moisLettre,$moisReplace);
-	$dateFormatee=$date[2].' '.$mois.' '.$date[0];
+	$dateFormatee=$date[2].' '.$moisLettre[($date[1] - 1)].' '.$date[0];
 	return $dateFormatee;
 }
-
+/************FORMATAGE HEURE EM*************/
+function formatHeure($heure){
+	$heure=explode(':', $heure);
+	$heureFormatee=$heure[0].'h'.$heure[1];
+	return $heureFormatee;
+}
 /*****************CPT******************/
 /*pieces*/
 function wpm_custom_post_type() {
@@ -86,17 +89,25 @@ function wpm_custom_post_type() {
 		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
 	);
 
+	// On peut définir ici d'autres options pour notre custom post type
+
 	$args = array(
 		'label'               => __( 'Pièces'),
 		'description'         => __( 'Tous sur nos pièces'),
 		'labels'              => $labels,
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
 		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+		/*
+		* Différentes options supplémentaires
+		*/
 		'hierarchical'        => false,
 		'public'              => true,
 		'has_archive'         => true,
 		'rewrite'			  => array( 'slug' => 'pieces'),
 
 	);
+
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
 	register_post_type( 'pieces', $args );
 
 }
