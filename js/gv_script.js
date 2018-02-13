c24fbce4e44e67de98da29c0eb1021c6e3d7df70
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-13 16:32:38 +0100

    Update gv_script.js

--- js/gv_script.js
+++ js/gv_script.js
@@ -1,1 +1,11 @@
 /*Fichier pour javascript custom */
+jQuery(document).ready(function(){
+	jQuery('.carousel-pieces').slick({
+		dots:true,
+		infinite:true,
+		speed:300,
+		slidesToShow:1,
+		centerMode:true,
+		variableWidth:true
+	});
+});
