9dfcee18e4580c8f27c9b59ccd9f4ee06247a658
Luu-Steven <33942988+Luu-Steven@users.noreply.github.com>
2018-02-16 21:25:50 +0100

    Update page-nous.php
    updated nous.php :
    - Images are now with 2 custom sizes.

--- page-nous.php
+++ page-nous.php
@@ -1,90 +1,95 @@
 <?php
 /*
-Template Name: NousT
+Template Name: Nous
 */
 get_header();
 ?>
 <!-- -->
 <section id="nous">
     <div id="a-la-une">
-        <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'class' => 'img-responsive') ); } ?>
+        <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'headerPieces', array( 'class' => 'img-responsive') ); } ?>
         <?php // 'thumbnail', array( 'class' => 'img-responsive attachment-post-thumbnail size-post-thumbnail') ?>
     </div>
 
-    <ul class="nav nav-pills nav-justified" id="myTab" role="tablist">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li role="presentation" class="active nav-item">
-            <a href="#compagnie" class="nav-link" data-toggle="tab" role="tab" aria-controls="compagnie">La Compagnie</a>
+            <a href="#compagnie" data-toggle="tab" role="tab" aria-controls="compagnie">La Compagnie</a>
         </li>
         <li role="presentation" class="nav-item">
-            <a href="#contacts" class="nav-link" data-toggle="tab" role="tab" aria-controls="contacts">Contacts</a>
+            <a href="#contacts" data-toggle="tab" role="tab" aria-controls="contacts">Contacts</a>
         </li>
         <li role="presentation" class="nav-item">
-            <a href="#suivre" class="nav-link" data-toggle="tab" role="tab" aria-controls="suivre">Nous Suivre</a>
+            <a href="#suivre" data-toggle="tab" role="tab" aria-controls="suivre">Nous Suivre</a>
         </li>
         <li role="presentation" class="nav-item">
-            <a href="#partenaires" class="nav-link" data-toggle="tab" role="tab" aria-controls="partenaires">Partenaires</a>
+            <a href="#partenaires" data-toggle="tab" role="tab" aria-controls="partenaires">Partenaires</a>
         </li>
         <li role="presentation" class="nav-item">
-            <a href="#doucet" class="nav-link" data-toggle="tab" role="tab" aria-controls="doucet">Guillaume Doucet</a>
+            <a href="#doucet" data-toggle="tab" role="tab" aria-controls="doucet">Guillaume Doucet</a>
         </li>
     </ul>
 
     <!-- Tab panes -->
     <div class="tab-content">
-        <div class="tab-pane active" id="compagnie" role="tabpanel">
+        <div class="tab-pane active fade in" id="compagnie" role="tabpanel">
             <?php if( have_rows('compagnie') ):
                 while ( have_rows('compagnie') ) : the_row(); ?>
                     <h2><?php the_sub_field('compagnie_titre'); ?></h2>
                     <div><?php the_sub_field('compagnie_texte'); ?></div>
                     <figure>
                         <?php $image = get_sub_field('compagnie_image');
+                                  $size = 'thumbNous';
                         if($image) {
-                            echo wp_get_attachment_image($image);
+                            echo wp_get_attachment_image($image, $size);
                         } ?>
                     </figure>
                 <?php endwhile;
             endif; ?>
         </div>
-        <div class="tab-pane" id="contacts" role="tabpanel">
+        <div class="tab-pane fade in" id="contacts" role="tabpanel">
             <?php if( have_rows('contacts') ):
                 while ( have_rows('contacts') ) : the_row(); ?>
                     <div><?php the_sub_field('contacts_liens'); ?></div>
                     <figure>
                         <?php $image = get_sub_field('contacts_logo');
+                                  $size = 'thumbNous';
                         if($image) {
-                            echo wp_get_attachment_image($image);
+                            echo wp_get_attachment_image($image, $size);
                         } ?>
                     </figure>
                 <?php endwhile;
             endif; ?>
         </div>
-        <div class="tab-pane" id="suivre" role="tabpanel">
+        <div class="tab-pane fade in" id="suivre" role="tabpanel">
             <?php if( have_rows('suivre') ):
                 while ( have_rows('suivre') ) : the_row(); ?>
                     <div><?php the_sub_field('suivre_liens'); ?></div>
                     <figure>
                         <?php
                         $imagefacebook = get_sub_field('suivre_facebook');
+                        $size = 'thumbNous';
                         if($imagefacebook) {
                             ?>
                             <a href="https://fr-fr.facebook.com/legroupe.vertigo/">
-                            <?php echo wp_get_attachment_image($imagefacebook); ?>
+                            <?php echo wp_get_attachment_image($imagefacebook, $size); ?>
                             </a>
                             <?php
                         }
                         $imagetwitter = get_sub_field('suivre_twitter');
+                        $size = 'thumbNous';
                         if($imagetwitter) {
                             ?>
                             <a href="https://twitter.com/legroupevertigo?lang=fr">
-                            <?php echo wp_get_attachment_image($imagetwitter); ?>
+                            <?php echo wp_get_attachment_image($imagetwitter, $size); ?>
                             </a>
                             <?php
                         }
                         $imageinstagram = get_sub_field('suivre_instagram');
+                        $size = 'thumbNous';
                         if($imageinstagram) {
                             ?>
                             <a href="#">
-                            <?php echo wp_get_attachment_image($imageinstagram); ?>
+                            <?php echo wp_get_attachment_image($imageinstagram, $size); ?>
                             </a>
                             <?php
                         }
@@ -93,7 +98,7 @@ get_header();
                 <?php endwhile;
             endif; ?>
         </div>
-        <div class="tab-pane" id="partenaires" role="tabpanel">
+        <div class="tab-pane fade in" id="partenaires" role="tabpanel">
             <?php if( have_rows('partenaires') ):
                 while ( have_rows('partenaires') ) : the_row(); ?>
                     <div><?php the_sub_field('partenaires_texte'); ?></div>
@@ -101,8 +106,9 @@ get_header();
                         <?php if( have_rows('partenaires_logos') ):
                             while ( have_rows('partenaires_logos') ) : the_row();
                                 $image = get_sub_field('partenaires_logo');
+                                $size = 'logoNous';
                                 if($image) {
-                                    echo wp_get_attachment_image($image);
+                                    echo wp_get_attachment_image($image, $size);
                                 }
                             endwhile;
                         endif; ?>
@@ -112,8 +118,9 @@ get_header();
                             while ( have_rows('partenaires_bis') ) : the_row(); ?>
                                 <div><?php the_sub_field('partenaires_bis_lien'); ?></div>
                                 <?php $image = get_sub_field('partenaires_bis_logo');
+                                          $size = 'logoNous';
                                 if($image) {
-                                    echo wp_get_attachment_image($image);
+                                    echo wp_get_attachment_image($image, $size);
                                 }
                             endwhile;
                         endif; ?>
@@ -121,14 +128,15 @@ get_header();
                 <?php endwhile;
             endif; ?>
         </div>
-        <div class="tab-pane" id="doucet" role="tabpanel">
+        <div class="tab-pane fade in" id="doucet" role="tabpanel">
             <?php if( have_rows('doucet') ):
                 while ( have_rows('doucet') ) : the_row(); ?>
                     <figure>
                         <?php the_sub_field('doucet_legende'); ?>
                         <?php $image = get_sub_field('doucet_photo');
+                                  $size = 'thumbNous';
                         if($image) {
-                            echo wp_get_attachment_image($image);
+                            echo wp_get_attachment_image($image, $size);
                         } ?>
                     </figure>
                     <div>
