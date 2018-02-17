1ff6111de8e1279d37e5aa3f87573a780b6dc6c9
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-17 22:29:42 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
 <footer class="container">
      <div class="row">
-
-
+       <?php
+       $custom_logo_id = get_theme_mod( 'custom_logo' );
+        $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+        if ( has_custom_logo() ) {
+             echo '<img src="'. esc_url( $logo[0] ) .'">';
+        } else {
+             echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+        }
+       ?>
+       <nav id=footer-menu>
+         <ul>
+           <li><a href="#">Mentions légales</a></li>
+           <li><a href="#">Social networks</a></li>
+           <li><a href="#">A propos</a></li>
+         </ul>
      </div>
 </footer>
 <?php wp_footer(); ?>
