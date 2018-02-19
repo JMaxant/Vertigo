82632c91c5a47af63c8fa42df4a051095a049fc8
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-19 22:59:33 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,51 +1,47 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="witdh=device-width, initial-scale=1">
-	<?php if(is_home()): ?>
-		<meta name="description" content="Lorem ipsum dolor" />
-	<?php endif;?>
-	<?php if(is_front_page()): ?>
-		<meta name="description" content="Lorem ipsum dolor" />
-	<?php endif;?>
-	<?php if(is_front_page() && is_page()): ?>
-		<meta name="description" content="Lorem ipsum dolor" />
-	<?php endif;?>
-	<?php if(is_single()): ?>
-		<meta name="description" content="Lorem ipsum dolor" />
-	<?php endif;?>
-	<?php if(is_page()): ?>
-		<meta name="description" content="Lorem ipsum dolor" />
-	<?php endif;?>
-
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header id="nav">
-	<nav>
-		<div class="container-fluid">
-       <div class="navbar-header">
-         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topLeft" aria-expanded="false" aria-controls="navbar">
-           <span class="sr-only">Toggle navigation</span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-         </button>
-       </div>
-			 <?php if ( function_exists( 'the_custom_logo' ) ) {
-    the_custom_logo(); }
+<header class="container-fluid" id="nav">
+	<nav class="row">
+		<div class="col-sm-11"
+       		<div class="navbar-header">
+         			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topLeft" aria-expanded="false" aria-controls="navbar">
+			           <span class="sr-only">Toggle navigation</span>
+			           <span class="icon-bar"></span>
+			           <span class="icon-bar"></span>
+			           <span class="icon-bar"></span>
+		         </button>
+		     </div>
+	<?php if(function_exists('the_custom_logo')){
+    			the_custom_logo();
+		};
 		wp_nav_menu( array(
 			'menu' =>'top-menu',
 			'theme_location' => 'primary',
 			'depth' => 2,
 			'container' => 'div',
-			'container_class' => 'navbar navbar-collapse collapse',
+			'container_class' => 'navbar navbar-collapse collapse col-sm-10',
 			'container_id' => 'topLeft',
 			'menu_class' => 'nav navbar-nav navbar-left',
 			'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
 			'walker' => new wp_bootstrap_navwalker()
-		));?>
+		)); ?>
+		</div>
+		<div class="col-sm-1">
+			<?php wp_nav_menu( array(
+			'menu' =>'top-menu',
+			'theme_location' => 'secondary',
+			'depth' => 1,
+			'container' => 'div',
+			'container_class' => 'navbar',
+			'container_id' => 'topRight',
+			'menu_class' => 'nav navbar-right',
+			'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+			'walker' => new wp_bootstrap_navwalker()
+		)); ?>
 		</div>
 	</nav>
 </header>
