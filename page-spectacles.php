58313a4245af58bbd1485d666eceeb1dd6b570f7
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-20 15:22:30 +0100

    Update page-spectacles.php

--- page-spectacles.php
+++ page-spectacles.php
@@ -4,26 +4,30 @@ Template Name: Spectacles
 */
 ?>
 <?php get_header(); ?>
-<section id="spectacles"  class="container">
-    	<div class="row grid">
+<main id="spectacles"  class="container">
+    	<section class="col-sm-12">
         <?php
         $pieces = new WP_Query(array(
             	'post_type' => 'pieces',
         ));
         if($pieces->have_posts()):?>
-        <?php while($pieces->have_posts()): $pieces->the_post();?>
-    		<figure class=" effect-julia col-sm-4">
-			<a href="<?php the_permalink();?>" ><?php the_post_thumbnail('thumbSpectacles');?></a>
-			<figcaption>
-				<h2><?php the_title(); ?></h2>
-				<p><?php the_excerpt(); ?>
-					<a href="<?php the_permalink(); ?>">Voir plus</a>
-				</p>
-			</figcaption>
-		</figure>
-        <?php endwhile;?>
+        		<div class="grid">
+			<div class="grid-sizer"></div> <!--CONTENEUR VIDE UTILISE PAR MASONRY JS-->
+			<div class="grid-item">
+	          <?php while($pieces->have_posts()): $pieces->the_post(); ?>
+		    		<figure class="effect-julia">
+					<a href="<?php the_permalink();?>" ><?php the_post_thumbnail('thumbSpectacles');?></a>
+					<figcaption>
+						<h2><?php the_title(); ?></h2>
+						<a href="<?php the_permalink(); ?>">p><?php the_excerpt(); ?></a>
+						</p>
+					</figcaption>
+				</figure>
+			</div>
+		<?php endwhile;?>
+		</div>
 <?php endif; ?>
-    	</div>
-</section>
+	</section>
+</main>
 
 <?php get_footer();?>
