42692900daecb82f5cdcd06d82f5336a759960c7
JMaxant <33941112+JMaxant@users.noreply.github.com>
2018-02-22 16:06:22 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header class="container-fluid" id="nav">
 	<nav class="row">
-		<div class="col-sm-11"
-       		<div class="navbar-header">
-         			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topLeft" aria-expanded="false" aria-controls="navbar">
-			           <span class="sr-only">Toggle navigation</span>
-			           <span class="icon-bar"></span>
-			           <span class="icon-bar"></span>
-			           <span class="icon-bar"></span>
-		         </button>
-		     </div>
+		<div class="col-sm-12">
+       			<div class="navbar-header">
+	         			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topLeft" aria-expanded="false" aria-controls="navbar">
+				           <span class="sr-only">Toggle navigation</span>
+				           <span class="icon-bar"></span>
+				           <span class="icon-bar"></span>
+				           <span class="icon-bar"></span>
+			         </button>
+		     	</div>
 	<?php if(function_exists('the_custom_logo')){
     			the_custom_logo();
 		};
@@ -23,25 +24,15 @@
 			'theme_location' => 'primary',
 			'depth' => 2,
 			'container' => 'div',
-			'container_class' => 'navbar navbar-collapse collapse col-sm-10',
+			'container_class' => 'navbar navbar-collapse collapse',
 			'container_id' => 'topLeft',
 			'menu_class' => 'nav navbar-nav navbar-left',
 			'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
 			'walker' => new wp_bootstrap_navwalker()
-		)); ?>
-		</div>
-		<div class="col-sm-1">
-			<?php wp_nav_menu( array(
-			'menu' =>'top-menu',
-			'theme_location' => 'secondary',
-			'depth' => 1,
-			'container' => 'div',
-			'container_class' => 'navbar',
-			'container_id' => 'topRight',
-			'menu_class' => 'nav navbar-right',
-			'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-			'walker' => new wp_bootstrap_navwalker()
-		)); ?>
+		));
+		if(function_exists('dynamic_sidebar')){
+			dynamic_sidebar( 'Widgets header' );
+		}?>
 		</div>
 	</nav>
 </header>
