61d0ab90be2ccbb664ef26b2da9e5fc418e818c6
Luu-Steven <33942988+Luu-Steven@users.noreply.github.com>
2018-02-28 16:28:22 +0100

    Update category-unused.php

--- category-unused.php
+++ category-unused.php
@@ -1,23 +1,45 @@
 <?php get_header(); ?>
+
 	<section>
+
 		<div class="container">
+
 			<div class="row">
+
 				<p class="lead">Archives de la catégorie <?php single_cat_title('', true); ?></p>
+
 			</div>
+
 		</div>
+
 		<div class="container">
+
 			<?php if(have_posts()): ?>
+
 				<?php while(have_posts()):the_post(); ?>
+
 					<?php get_template_part('content');?>
+
 				<?php endwhile;?>
+
 				<?php else: ?>
+
 			<div class="row">
+
 				<div class="col-xs-12">
+
 					<p>Pas de résultat</p>
+
 				</div>
+
 			</div><!--row-->
+
 		<?php endif; ?>
+
 		</div><!--Container-->
+
 	</section>
+
 <?php get_footer();?>
+
 </html>
